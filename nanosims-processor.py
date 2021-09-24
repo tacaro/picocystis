afb9b6064368d1f2dfb5496b69493ae9be285dc5
drutter <bajenak@gmail.com>
2021-09-23 17:56:52 -0700

    autosegmentation mark1

--- nanosims-processor.py
+++ nanosims-processor.py
@@ -7,21 +7,23 @@ import imageio
 import numpy as np
 import sims
 from scipy import ndimage
-from skimage import measure
-from skimage import transform
+from skimage import measure, transform, morphology, segmentation, filters, color
 import matplotlib
+from matplotlib import colors
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-from matplotlib import colors
 
-parser = argparse.ArgumentParser(description = 'Tool for parsing Cameca NanoSIMS .im files to produce ratios. \
+TK_SILENCE_DEPRECATION=1
+
+parser = argparse.ArgumentParser(description='Tool for parsing Cameca NanoSIMS .im files to produce ratios. \
                                  Basic usage is to read a .im file, and produce single-channel PNGs and/or ratios of \
                                  isotope_1 / (isotope_1 + isotope_2). Also can read in a .png as a mask for ROIs.')
 group1 = parser.add_argument_group('BASIC OPTIONS', '')
 group2 = parser.add_argument_group('PLOTTING OPTIONS', '')
 group3 = parser.add_argument_group('ROI OPTIONS', '')
 
-group1.add_argument('-i', '--input', metavar='PATH', required=True,
+group1.add_argument('-i', '--input', metavar='PATH',  # required=True,
+                    default='GB21_L2_NH4_light_chain1_1.im',
                     dest='input', help='File for input (a .im file)')
 group1.add_argument('-o', '--output', metavar='PATH',
                     default=None, help='Prefix for output [default: --input without the .im ending]')
@@ -34,7 +36,7 @@ group1.add_argument('-f', '--frame', metavar='INT', type=int, default=None,
                     help='Frame index (e.g., "0" would be the first frame) to focus, \
                     default behavior is to loop through all [default: None]')
 group1.add_argument('-F', '--filter_method', metavar='STRING', default=None,
-                    help='Add a filter step, currently confined to one of "gaussian", "median" [default: None]')
+                    help='Add a filter step, currently confined to one of "gaussian", "median", "tophat" [default: None]')
 group1.add_argument('-s', '--sigma', metavar='FLOAT', type=float, default=1.0,
                     help='Parameter for filtration (sigma for gaussian, size for median) [default: 1.0]')
 group2.add_argument('--rough', action='store_true',
@@ -48,45 +50,67 @@ group3.add_argument('-r', '--roi', metavar='PATH', default=None,
 group3.add_argument('--count_stat', metavar='STRING', default='mean',
                     help='Method for aggregating pixel values in each ROI to report in the summary table. \
                     Does not affect ratios. Current options: mean, sum [default: mean]')
+group3.add_argument('-a', '--autoroi', default=False, action='store_true',
+                    help='Auto-generate an ROI by attempting to segment the image with Otsu + watershed method. \
+                    Only saves data in the absence of the --roi flag (to not overwrite user-specified ROIs).')
+group3.add_argument('-M', '--min_area', default=100, metavar='INT',
+                    help='Minimum area for --autoroi to recognize a region (to cut down on tiny bright specks/debris)')
+group3.add_argument('--otsu_mult_high', default=1.2, metavar='FLOAT', type=float,
+                    help='The --autoroi uses Otsu thresholding to determing "object" and "background" intensities, so \
+                         this value is multiplied by the Otsu threshold to determine the UPPER bound')
+group3.add_argument('--otsu_mult_low', default=0.5, metavar='FLOAT', type=float,
+                    help='The --autoroi uses Otsu thresholding to determing "object" and "background" intensities, so \
+                         this value is multiplied by the Otsu threshold to determine the LOWER bound')
 
 args = parser.parse_args()
 
+
 def apply_filter(img, filt_method, sigma):
     if filt_method == 'gaussian':
-        for frame in img.frame.values:
+        for f in img.frame.values:
             for specie in img.species.values:
-                img.loc[specie, frame,: ,:] = ndimage.gaussian_filter(img.loc[specie, frame].data, sigma=sigma)
+                img.loc[specie, f, :, :] = ndimage.gaussian_filter(img.loc[specie, f].data, sigma=sigma)
         return img
     if filt_method == 'median':
-        for frame in img.frame.values:
+        for f in img.frame.values:
             for specie in img.species.values:
-                img.loc[specie, frame, :, :] = ndimage.median_filter(img.loc[specie, frame].data, size=int(sigma))
+                img.loc[specie, f, :, :] = ndimage.median_filter(img.loc[specie, f].data, size=int(sigma))
+    if filt_method == 'tophat':
+        for f in img.frame.values:
+            for specie in img.species.values:
+                footprint = morphology.disk(1)
+                res = morphology.white_tophat(img.loc[specie, f].data, footprint)
+                img.loc[specie, f, :, :] = img.loc[specie, f].data - res
+
     return img
 
+
 def rough_plot(img):
-    for frame in img.frame.values:
+    for f in img.frame.values:
         for specie in img.species.values:
-            plt.imshow(img.loc[specie, frame], cmap='gray', interpolation='none')
+            plt.cla()
+            plt.imshow(img.loc[specie, f], cmap='gray', interpolation='none')
             plt.gray()
             plt.axis('off')
             plt.colorbar()
             plt.title(specie)
             plt.show()
 
+
 def save_plot(img):
-    for frame in img.frame.values:
+    for f in img.frame.values:
         for specie in img.species.values:
             plt.cla()
-            plt.imshow(img.loc[specie, frame], cmap='gray', interpolation='none')
+            plt.imshow(img.loc[specie, f], cmap='gray', interpolation='none')
             plt.gray()
             plt.axis('off')
             plt.colorbar()
             plt.title(specie)
-            plt.imsave(fname=args.output + "_f" + str(frame) + "_" + specie + ".png",
-                       arr=aligned_image.loc[specie, frame], cmap='gray')
+            plt.imsave(fname=args.output + "_f" + str(f) + "_" + specie + ".png",
+                       arr=aligned_image.loc[specie, f], cmap='gray')
+
 
 def get_image_from_raw_rois(roi, im):
-
     if roi.shape[0:2] == im.data.shape[2:4]:
         print("The ROI png has the same pixel dimension as the image, great! [no ROI cropping]")
     elif roi.shape[0] > im.data.shape[2]:
@@ -116,6 +140,7 @@ def get_image_from_raw_rois(roi, im):
 
     return roi
 
+
 def parse_ROIs(objects, grp_col, c1, c2, annotated_im, im, stats):
     for obj in range(objects[1]):
         obj_x, obj_y = np.where(objects[0] == (obj + 1))
@@ -131,6 +156,10 @@ def parse_ROIs(objects, grp_col, c1, c2, annotated_im, im, stats):
             all_counts = [x for x in np.array([im.data[:, x, y] for x, y in pts]).sum(axis=0)]
         elif args.count_stat == 'sum':
             all_counts = [x for x in np.array([im.data[:, x, y] for x, y in pts]).mean(axis=0)]
+        else:
+            print("Somehow there is no stat? fix the code so args.count_stat is not: " + args.count_stat)
+            all_counts = None
+            exit()
 
         rat_im = counts1/(counts1 + counts2)
         annotated_im[obj_x, obj_y] = rat_im
@@ -142,9 +171,45 @@ def parse_ROIs(objects, grp_col, c1, c2, annotated_im, im, stats):
         stats.append(obj_stats)
     return annotated_im, stats
 
-### BODY ###
+
+def autosegment(full_img, low_mult=args.otsu_mult_low, high_mult=args.otsu_mult_high, too_small=args.min_area):
+    img = full_img.data[np.where(full_img.species.values == 'SE')[0][0], 0, :, :]
+    img = 255*(img/img.max())
+
+    thresh = filters.threshold_otsu(img)
+
+    markers = np.zeros_like(img)
+    markers[img < low_mult*thresh] = 1
+    markers[img > (high_mult*thresh)] = 2
+
+    # plt.imshow(markers, cmap='Greys_r', interpolation=None)
+    # plt.show()
+
+    # segment
+    label_image = segmentation.watershed(filters.sobel(img), markers) - 1
+    # drop segments with small area
+    label_image = morphology.area_opening(label_image, area_threshold=too_small)
+
+    auto_rois = measure.label(label_image, return_num=True)
+
+    if not args.no_show:
+        plt.clf()
+        plt.imshow(img, cmap='Greys_r', interpolation=None)
+        plt.imshow(color.label2rgb(label_image, bg_label=0), interpolation=None, alpha=0.3)
+        for reg in measure.regionprops(auto_rois[0]):
+            ry, rx = reg.centroid
+            plt.text(rx + 0, ry - 0, s=reg.label, ha='center', va='center', bbox=dict(fc='white', ec='none', pad=2))
+        plt.savefig(fname=args.output + "_auto_roi_watershed.png")
+        plt.show()
+
+    return auto_rois
+
+
+##############
+#    BODY    #
+##############
 image = sims.SIMS(args.input)
-aligned_image, shifts = sims.utils.align(image)
+aligned_image, shifts = sims.utils.align(image)  # account for inter-frame shifts
 
 if args.compare1 not in aligned_image.species:
     print("Your --compare1 value is not in the .im file? Did you put an underscore in? Available names:\n" +
@@ -160,7 +225,7 @@ aligned_image = aligned_image.drop_isel(x=[0, -1], y=[0, -1])
 
 if args.frame is not None:
     print('subsetting to frame ' + str(args.frame))
-    aligned_image = aligned_image.loc[:, [args.frame], : , :]
+    aligned_image = aligned_image.loc[:, [args.frame], :, :]
 if args.filter_method:
     aligned_image = apply_filter(img=aligned_image, filt_method=args.filter_method, sigma=args.sigma)
 if args.output is None:
@@ -169,21 +234,27 @@ if args.rough:
     rough_plot(aligned_image)
     exit()
 
-try:
+
+# PREP STATS
+stats_table = list()
+annotated_image = np.zeros(aligned_image.data.shape[2:4])
+
+# PARSE ROIS OR AUTOSEGMENT TO GENERATE ROIS
+if args.autoroi:
+    auto_objects = autosegment(full_img=aligned_image)
+
+if args.roi is not None:
     rois = imageio.imread(args.roi, pilmode='RGB')
 
     # hack becasue DPI is different between saved png and imported roi
     rois = get_image_from_raw_rois(roi=rois, im=aligned_image)
 
-    # set up blank annotated image to be drawn upon
-    annotated_image = np.zeros(rois.shape[0:2])
-
     # 3 groups of rois - red, green, blue
-    red_rois = rois[:,:,0]-rois[:,:,2]
+    red_rois = rois[:, :, 0]-rois[:, :, 2]
     red_rois = 1*(red_rois > 200)
-    green_rois = rois[:,:,1]-rois[:,:,2]
+    green_rois = rois[:, :, 1]-rois[:, :, 2]
     green_rois = 1*(green_rois > 200)
-    blue_rois = rois[:,:,2]-rois[:,:,1]
+    blue_rois = rois[:, :, 2]-rois[:, :, 1]
     blue_rois = 1*(blue_rois > 200)
 
     # segment by contiguous colors by category
@@ -216,15 +287,13 @@ try:
     if not args.no_show:
         plt.show()
     plt.cla()
+elif not args.whole_image and not args.autoroi:
+    print("No ROI, saving pngs for each channel so you can use for making ROIs (or rerun with -r),\n \
+    OR use with --autoroi feature (running now so you can see what it looks like)")
+    save_plot(aligned_image)
+    auto_objects = autosegment(full_img=aligned_image)
 
-    stats_table = list()
-
-except OSError:
-    if not args.whole_image:
-        print('No ROI, saving pngs for each channel so you can use for making ROIs (or rerun with -r)')
-        save_plot(aligned_image)
-
-
+# PROCESS ROIS COMPUTED ABOVE
 for frame in range(aligned_image.data.shape[1]):
     # calculate ratio of desired vs (desired + ref)
     comp1 = aligned_image.loc[args.compare1, frame, :, :]
@@ -237,53 +306,58 @@ for frame in range(aligned_image.data.shape[1]):
         plt.cla()
         plt.imshow(ratio_image, interpolation='none', cmap='cividis')
         plt.axis('off')
-        plt.colorbar().set_label("Fraction (per ROI)")
-        plt.title(sims.utils.format_species(args.compare1) + " / (" + sims.utils.format_species(args.compare1)+" + "+
+        plt.colorbar().set_label("Fraction: " + sims.utils.format_species(args.compare1) + " / (" +
+                                 sims.utils.format_species(args.compare1)+" + " +
+                                 sims.utils.format_species(args.compare2) + ")")
+        plt.title(sims.utils.format_species(args.compare1) + " / (" + sims.utils.format_species(args.compare1) + " + " +
                   sims.utils.format_species(args.compare2) + ")")
         plt.savefig(fname=args.output + "_whole_f" + str(frame) +
-                          "_ratio" + re.sub(" ", "_", args.compare1) +"-x-"+ re.sub(" ", "_", args.compare2) + ".png")
+                          "_ratio" + re.sub(" ", "_", args.compare1) + "-x-" + re.sub(" ", "_", args.compare2) + ".png")
         if not args.no_show:
             plt.show()
+        if not (args.roi or args.autoroi):
+            continue
 
     if args.roi:
         try:
             annotated_image, stats_table = parse_ROIs(objects=red_objects, grp_col='red', c1=comp1, c2=comp2,
                                                       im=aligned_image.loc[:, frame, :, :],
                                                       annotated_im=annotated_image, stats=stats_table)
-        except TypeError:
+        except (TypeError, NameError):
             print("No red, continuing")
         try:
             annotated_image, stats_table = parse_ROIs(objects=green_objects, grp_col='green', c1=comp1, c2=comp2,
                                                       im=aligned_image.loc[:, frame, :, :],
                                                       annotated_im=annotated_image, stats=stats_table)
-        except TypeError:
+        except (TypeError, NameError):
             print("No green, continuing")
         try:
             annotated_image, stats_table = parse_ROIs(objects=blue_objects, grp_col='blue', c1=comp1, c2=comp2,
                                                       im=aligned_image.loc[:, frame, :, :],
                                                       annotated_im=annotated_image, stats=stats_table)
-        except TypeError:
+        except (TypeError, NameError):
             print("No blue huh?")
+    elif args.autoroi or not args.whole_image:
+            annotated_image, stats_table = parse_ROIs(objects=auto_objects, grp_col='auto', c1=comp1, c2=comp2,
+                                                      im=aligned_image.loc[:, frame, :, :],
+                                                      annotated_im=annotated_image, stats=stats_table)
 
+    stats_columns = ["Group", "ROI"]
+    stats_columns.extend(aligned_image.species.values)
+    stats_columns.append("Ratio_" + args.compare1 + "x" + args.compare2)
+    stats_columns = [re.sub(" ", "_", x) for x in stats_columns]
+    stats_table = pd.DataFrame(stats_table, columns=stats_columns)
 
-        stats_columns = ["Group", "ROI"]
-        stats_columns.extend(aligned_image.species.values)
-        stats_columns.append("Ratio_" + args.compare1 + "x" + args.compare2)
-        stats_columns = [re.sub(" ","_", x) for x in stats_columns]
-        stats_table = pd.DataFrame(stats_table, columns=stats_columns)
-
-        stats_table.to_csv(args.output + "_f0" +
-                           "_ratio" + re.sub(" ", "_", args.compare1) +"-x-" + re.sub(" ", "_", args.compare2) +
-                           ".tsv", sep="\t", index=False)
-        plt.clf()
-        plt.imshow(annotated_image, interpolation='none', cmap='gray')
-        plt.axis('off')
-        plt.colorbar().set_label("Fraction (per ROI)")
-        plt.title(sims.utils.format_species(args.compare1) + " / (" + sims.utils.format_species(args.compare1)+" + "+
-                  sims.utils.format_species(args.compare2) + ")")
-        plt.savefig(fname=args.output + "_f" + str(frame) +
-                          "_ratio" + re.sub(" ", "_", args.compare1) +"-x-"+ re.sub(" ", "_", args.compare2) + ".png")
-        if not args.no_show:
-            plt.show()
-
-
+    stats_table.to_csv(args.output + "_f0" +
+                       "_ratio" + re.sub(" ", "_", args.compare1) + "-x-" + re.sub(" ", "_", args.compare2) +
+                       ".tsv", sep="\t", index=False)
+    plt.clf()
+    plt.imshow(annotated_image, interpolation='none', cmap='gray')
+    plt.axis('off')
+    plt.colorbar().set_label("Fraction (per ROI)")
+    plt.title(sims.utils.format_species(args.compare1) + " / (" + sims.utils.format_species(args.compare1)+" + " +
+              sims.utils.format_species(args.compare2) + ")")
+    plt.savefig(fname=args.output + "_f" + str(frame) +
+                      "_ratio" + re.sub(" ", "_", args.compare1) + "-x-" + re.sub(" ", "_", args.compare2) + ".png")
+    if not args.no_show:
+        plt.show()
